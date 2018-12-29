fe036c9cb6b53a348f4cb61f0caf755494b5f411
Jefferson Silva <jgsnat@gmail.com>
2018-12-29 10:21:41 -0300

    - review react and first import;

---
+++ hackernews/src/App.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from './components/Button/Button';
+import axios from 'axios';
+
+/**
+ * Components
+ */
+import Search from './components/Search';
+import Table from  './components/Table';
+
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP,
+} from './utils/index';
+
+class App extends Component {
+
+  __isMounted = false;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null
+    };
+
+    /**
+     * Bindings class methods
+     */
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(res => this.__isMounted && this.setSearchTopStories(res.data))
+      .catch(err => this.__isMounted && this.setState({ error: err }));
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    event.preventDefault();
+  }
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      results: { 
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  onSearchChange(e) {
+    const { value } = e.target;
+    this.setState({ searchTerm: value });
+  }
+
+  onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+    this.setState({ 
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  render() {
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
+
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        { error 
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table 
+              list={list}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            />
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.__isMounted = true;
+
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  componentWillUnmount() {
+    this.__isMounted = false;
+  }
+}
+
+export default App;
---
+++ hackernews/src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = ({ value, onChange, onSubmit, children }) => 
+    <form onSubmit={onSubmit}>
+        <input 
+            type="text"
+            onChange={onChange}
+            value={value}
+        />
+
+        <button type="submit">
+            {children}
+        </button>
+    </form>
+
+export default Search;
